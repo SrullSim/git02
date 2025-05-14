570db2d3eb5d2f1380a73d4413e57cb01aa855bd
srullsim <simchonisrael@gmail.com>
2025-05-14 12:46:28 +0300

    conflict

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("i did it");
+            int fg = 50;
             Console.WriteLine("for exemple");
             Console.WriteLine("its a litele try");
         }
